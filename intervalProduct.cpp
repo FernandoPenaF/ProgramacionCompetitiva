61db55053497bf62a4d049f4196376f2fee6994d
Luis Fernando <Fernando@MacBook-Pro-de-Luis.local>
2017-10-08 16:49:51 -0500

    Some changes and test to the code

--- intervalProduct.cpp
+++ intervalProduct.cpp
@@ -2,44 +2,50 @@
 #include <climits>
 using namespace std;
 
-int a[100000], n;
-int m[400000];
+int A[100000], n;
+int st[400000];
 
-void build(int x, int l, int r){ //build(0, 0, n - 1)
-	if(l == r){
-		m[x] = a[l];
-		return;
+void build( int p, int L, int R ) {
+		if( L == R ) st[p] = A[L]; 
+		else {
+			build( 2 * p + 1, L, ( L + R )/2 );
+			build( 2 * p + 2, ( L + R )/2 + 1, R );
+			st[p] = st[ 2 * p + 1 ] * st[ 2 * p + 2 ];
+		}
 	}
-	build(2*x + 1, l, (l + r) / 2);
-	build(2*x + 2, (l + r) / 2 + 1, r);
-	m[x] = m[2*x + 1] * m[2*x + 2];
+
+int update( int p, int L, int R, int id, int v ) {
+	if( id > R || id < L ) return st[p];
+	if( L == id && R == id ) {
+		A[L] = v;
+		return st[p] = A[L];
+	}
+	return st[p] =  update( 2 * p + 1, L, ( L + R )/2, id, v ) * update( 2 * p + 2, ( L + R )/2 + 1, R, id, v );
 }
 
-int query(int x, int l, int r, int i, int j){
-	if(r < i || j < l)
+int query( int p, int L, int R, int i, int j ) {
+	if( i > R || j < L ) 
 		return INT_MAX;
-	if(i <= l && r <= j)
-		return m[x];
-	return query(2*x + 1, l, (l + r) / 2, i, j) * query(2*x + 2, (l + r) / 2 + 1, r, i, j);
-}
-
-void update(int x, int l, int r, int i, int v){
-	if(i < l || r < i)
-		return;
-	if(l == r){
-		m[x] = v;
-		return;
-	}
-	update(2*x + 1, l, (l + r) / 2, i, v);
-	update(2*x + 1, (l + r)/ 2 + 1, r, i, v);
-	m[x] = m[2*x + 1] * m[2*x + 2];
+	if( L >= i && R <= j ) 
+		return st[p];
+	return query( 2 * p + 1, L, ( L + R )/2, i, j ) * query( 2 * p + 2, ( L + R )/2 + 1, R, i, j );
 }
 
 int main(int argc, char const *argv[]){
 	n = 4;
-	build(0, 0, n - 1);
-	
-	
-	
+	build(1, 0, n - 1);
+	update(1, 0, n - 1, 1 - 1, -2);
+	update(1, 0, n - 1, 2 - 1, 6);
+	update(1, 0, n - 1, 3 - 1, 0);
+	update(1, 0, n - 1, 4 - 1, -1);
+
+	update(1, 0, n - 1, 1 - 1, 10);
+
+	cout << query(1, 0, n - 1, 1 - 1, 4 - 1) << endl;
+	update(1, 0, n - 1, 3 - 1, 7);
+	cout << query(1, 0, n - 1, 2 - 1, 2 - 1) << endl;
+	update(1, 0, n - 1, 4 - 1, -5);
+	cout << query(1, 0, n - 1, 1 - 1, 4 - 1) << endl;
+	cout << "OK!" << endl;
 	return 0;
 }
