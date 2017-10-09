9629533eac9cbb3b917bed5e8a501bd463ac0762
Luis Fernando <Fernando@MacBook-Pro-de-Luis.local>
2017-10-08 19:55:31 -0500

    Interval product solved!!
    Segment tree implemented for any other compatible function to query.

--- intervalProduct.cpp
+++ intervalProduct.cpp
@@ -1,51 +1,72 @@
+/*
+* Interval Product - https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=3977
+* ID : 20141001
+*/
+
 #include <iostream>
 #include <climits>
 using namespace std;
 
-int A[100000], n;
-int st[400000];
+int a[100000], n;
+int m[400000];
 
-void build( int p, int L, int R ) {
-		if( L == R ) st[p] = A[L]; 
-		else {
-			build( 2 * p + 1, L, ( L + R )/2 );
-			build( 2 * p + 2, ( L + R )/2 + 1, R );
-			st[p] = st[ 2 * p + 1 ] * st[ 2 * p + 2 ];
-		}
-	}
-
-int update( int p, int L, int R, int id, int v ) {
-	if( id > R || id < L ) return st[p];
-	if( L == id && R == id ) {
-		A[L] = v;
-		return st[p] = A[L];
-	}
-	return st[p] =  update( 2 * p + 1, L, ( L + R )/2, id, v ) * update( 2 * p + 2, ( L + R )/2 + 1, R, id, v );
+int getSign(int x){
+	if(x > 0)
+		return 1;
+	else if (x < 0)
+		return -1;
+	return x;
 }
 
-int query( int p, int L, int R, int i, int j ) {
-	if( i > R || j < L ) 
+void build(int x, int l, int r) {
+	if(l == r){
+		m[x] = a[l];
+		return; 
+	}
+	build(2*x + 1, l, (l + r)/2);
+	build(2*x + 2, (l + r)/2 + 1, r);
+	m[x] = m[2*x + 1] * m[2*x + 2];
+}
+
+int query(int x, int l, int r, int i, int j) {
+	if(r < i || j < l) 
 		return INT_MAX;
-	if( L >= i && R <= j ) 
-		return st[p];
-	return query( 2 * p + 1, L, ( L + R )/2, i, j ) * query( 2 * p + 2, ( L + R )/2 + 1, R, i, j );
+	if(l >= i && r <= j)
+		return m[x];
+	return query(2*x + 1, l, (l + r) / 2, i, j) * query(2*x + 2, (l + r) / 2 + 1, r, i, j);
+}
+
+int update(int x, int l, int r, int i, int v) {
+	if(i < l || r < i) 
+		return m[x];
+	if(l == i && r == i) {
+		a[l] = v;
+		return m[x] = a[l];
+	}
+	return m[x] = update(2*x + 1, l, (l + r) / 2, i, v) * update(2*x + 2, (l + r) / 2 + 1, r, i, v);
 }
 
 int main(int argc, char const *argv[]){
-	n = 4;
-	build(1, 0, n - 1);
-	update(1, 0, n - 1, 1 - 1, -2);
-	update(1, 0, n - 1, 2 - 1, 6);
-	update(1, 0, n - 1, 3 - 1, 0);
-	update(1, 0, n - 1, 4 - 1, -1);
+	int k, value, a, b;
+	char c;
+	while(scanf("%d %d", &n, &k) != EOF){
+		build(1, 0, n - 1);
+		for (int i = 1; i <= n; i++){
+			scanf("%d", &value);
+			update(1, 0, n - 1, i - 1, getSign(value));
+		}
 
-	update(1, 0, n - 1, 1 - 1, 10);
+		for (int i = 0; i < k; i++){
+			scanf(" %c %d %d", &c, &a, &b);
 
-	cout << query(1, 0, n - 1, 1 - 1, 4 - 1) << endl;
-	update(1, 0, n - 1, 3 - 1, 7);
-	cout << query(1, 0, n - 1, 2 - 1, 2 - 1) << endl;
-	update(1, 0, n - 1, 4 - 1, -5);
-	cout << query(1, 0, n - 1, 1 - 1, 4 - 1) << endl;
-	cout << "OK!" << endl;
+			if(c == 'C')
+				update(1, 0, n - 1, a - 1, getSign(b));
+			else{
+				int resp = query(1, 0, n - 1, a - 1, b - 1);
+				cout << (resp == 0 ? '0' : (resp < 0 ? '-' : '+'));
+			}
+		}
+		cout << endl;
+	}
 	return 0;
 }
