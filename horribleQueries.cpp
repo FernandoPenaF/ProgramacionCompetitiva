b7210bcbe0d5288fa673c32ee03e7c98dfec249e
LUIS FERNANDO PEÑA FLORES <lpenaf@itam.mx>
2017-10-16 13:11:31 -0500

    Horribles queries solved!!

--- horribleQueries.cpp
+++ horribleQueries.cpp
@@ -1,42 +1,80 @@
+/*
+* Horrible Queries - http://www.spoj.com/problems/HORRIBLE/
+* ID : 20380610
+*/
+
 #include <iostream>
 using namespace std;
-long long arr[100000];
+typedef long long ll;
+
+ll tree[400001];
+ll lazy[400001];
 
 void init(){
-	for(unsigned i = 0; i < 100000; ++i) {
-		arr[i] = 0;
+	for(unsigned i = 0; i < 400001; i++) {
+		tree[i] = 0;
+		lazy[i] = 0;
 	}
 }
 
-void update(int i, int j, long long value){
-	for(int count = i; count <= j; count++) {
-		arr[count] += value;
-	}
+void updateRange(int node, int start, int end, int l, int r, ll val){
+    if(lazy[node] != 0){ 
+        tree[node] += (end - start + 1) * lazy[node];
+        if(start != end){
+            lazy[node * 2] += lazy[node];
+            lazy[node * 2 + 1] += lazy[node];
+        }
+        lazy[node] = 0;
+    }
+    if(start > end or start > r or end < l)
+        return;
+    if(start >= l and end <= r){
+        tree[node] += (end - start + 1) * val;
+        if(start != end){
+            lazy[node * 2] += val;
+            lazy[node * 2 + 1] += val;
+        }
+        return;
+    }
+    updateRange(node * 2, start, (start + end) / 2, l, r, val);
+    updateRange(node * 2 + 1, (start + end) / 2 + 1, end, l, r, val);
+    tree[node] = tree[node * 2] + tree[node * 2 + 1];
 }
 
-long long query(int i, int j){
-	long long suma = 0;
-	for(int count = i; count <= j; count++) {
-		suma += arr[count];
-	}
-	return suma;
+ll queryRange(int node, int start, int end, int l, int r){
+    if(start > end or start > r or end < l)
+        return 0;
+    if(lazy[node] != 0){
+        tree[node] += (end - start + 1) * lazy[node];
+        if(start != end){
+            lazy[node * 2] += lazy[node];
+            lazy[node * 2 + 1] += lazy[node];
+        }
+        lazy[node] = 0;
+    }
+    if(start >= l and end <= r)
+        return tree[node];
+    ll p1 = queryRange(node * 2, start, (start + end) / 2, l, r);
+    ll p2 = queryRange(node * 2 + 1, (start + end) / 2 + 1, end, l, r);
+    return p1 + p2;
 }
 
 int main(int argc, char const *argv[]){
-	int t, op, a, b, value, n, c;
+	int t, op, a, b, n, c;
+	ll value;
 	cin >> t;
 
 	for(unsigned i = 0; i < t; ++i) {
-		init();
 		cin >> n >> c;
+		init();
 		for(unsigned i = 0; i < c; ++i) {
 			scanf("%d %d %d", &op, &a, &b);
 
 			if(op == 0){
-				scanf(" %d", &value);
-				update(a, b, value);
+				scanf("%lld", &value);
+				updateRange(1, 0, n - 1, a - 1, b - 1, value);
 			} else {
-				cout << query(a, b) << endl;
+				cout << queryRange(1, 0, n - 1, a - 1, b - 1) << endl;
 			}
 		}
 	}
